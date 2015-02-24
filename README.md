486c071dc7561a960fad7c7bd58bf4584e6b49cd
stephanj79 <jaeger1779@gmail.com>
2015-02-24 11:07:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,228 @@
+
+# stylehistory.js
+
+> Creates a chronological sequence of events - Erstellt einen Chronologischen Ablauf von Ereignissen
+
+## Getting Started
+This plugin requires Jquery `jquery-1.11.2`
+> Test it with younger Versions of jquery!!!
+
+```js
+grunt.loadNpmTasks('grunt-exporter');
+```
+
+#How to use:
+```html
+           X=type(css/less/js...)   path/to/outputfile.
+<!--(start-X-export includes/ScrollTop.X)--> // Starttag
+  //Content here will be copy
+<!--(end-X-export)-->   // Endtag
+```
+
+## The "exporter" task
+
+### Overview
+In your project's Gruntfile, add a section named `exporter` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  exporter: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+});
+```
+
+### Options
+
+#### options.silent
+Type: `Boolean`
+Default value: `true`
+
+If 'false' output in console
+
+#### options.banner
+Type: `String`
+Default value: ``
+
+Banner for your files
+
+
+#### Default Options
+In this example, the default options are used to do something with whatever. So if the `testing.html` file has the content-tag the content of this tags will be written to file
+
+```js
+grunt.initConfig({
+     exporter: {
+       dist: {
+         options: {
+           silent: true,
+           banner: "/*\n * Export from\n * grunt-exporter\n * https://www.npmjs.com/package/grunt-exporter\n * https://github.com/stephanj79/grunt-exporter\n */\n\n"
+         },
+         files: {
+           src: ['test/fixtures/testing.html']
+         }
+       }
+     }
+});
+```
+
+#### Custom Options
+Script search in 'test/fixtures/testing.html' and in all but only *.html and *.js files in 'test/expected/'
+You can use css file or jade as well...
+
+
+```js
+grunt.initConfig({
+  exporter: {
+  dist:{
+      options: {
+        silent: false,
+        banner: "here my text..."
+    },
+    files: {
+      src: ['test/fixtures/testing.html', 'test/expected/**/*.html', '/test/expected/**/*.js']
+    }
+  },
+});
+```
+
+
+### Usage in File
+```html
+Starttag    X=type   path/to/outputfile.X
+<!--(start-X-export includes/ScrollTop.less)-->
+  //Content here will be copy
+<!--(end-X-export)-->
+^Endtag
+```
+**rename X with type like html,jade,js,css,scss,sass,less**
+
+
+### Usage Examples
+master.html
+```html
+
+<script>
+//<!--(start-js-export includes/test.js)-->
+alert("TEST");
+//<!--(end-js-export)-->
+</script>
+
+<header class="site-footer">
+  <!-- what ever... -->
+</header>
+
+<!--(start-less-export includes/ScrollTop.less)-->
+.header {
+  position: fixed;
+  a:link, a:visited,a:hover {
+    text-decoration: none;
+    background: @MainColorLink;
+    i{
+      color:@MainColorBackground;
+    }
+  }
+}
+<!--(end-less-export)-->
+
+
+<style>
+/*<!--(start-css-export includes/test.css)-->*/
+.site-footer-css {
+  min-height: 180px;
+}
+/*<!--(end-css-export)-->*/
+
+<!--(start-less-export includes/footer.less)-->
+.site-footer {
+  background: @MainColorBackground;
+  min-height: @footerHoehe;
+  a:link, a:visited {
+    color: @MainColorLink !important;
+  }
+}
+<!--(end-less-export)-->
+</style>
+
+<!--(start-html-export src/html/templates/footer.html)-->
+<footer class="test">
+    <a href="huiu" si="ujio">a</a>
+<footer>
+<!--(end-html-export)-->
+
+
+```
+
+Create files:
+* include/test.js
+```js
+alert("TEST");
+```
+* include/ScrollTop.less
+```css
+.scroll-top {
+  position: fixed;
+  a:link, a:visited,a:hover {
+    text-decoration: none;
+    background: @MainColorLink;
+    i{
+      color:@MainColorBackground;
+    }
+  }
+}
+```
+* include/test.css
+```css
+.site-footer-css {
+  min-height: 180px;
+}
+```
+
+**In js files you can use "//" or in css "/*" if you want!**
+```js
+//<!--(start-js-export includes/test.js)-->
+alert("TEST");
+//<!--(end-js-export)-->
+```
+```css
+/*<!--(start-css-export includes/test.css)-->*/
+.site-footer-css {
+  min-height: 180px;
+}
+/*<!--(end-css-export)-->*/
+```
+
+## Release History
+* 1.0.0 **Final Release**
+  ...
+  ...
+  ...
+* **Start Project**
+
+
+## Tips and Tricks
+use in html files
+```html
+<script>
+<!--(start-js-export includes/test.js)-->
+alert("TEST");
+<!--(end-js-export)-->
+</script>
+
+<style>
+<!--(start-css-export includes/test.css)-->*/
+.site-footer-css {
+  min-height: 180px;
+}
+<!--(end-css-export)-->*/
+</style>
+```
+
+## INFO
+* **If there a 2 files export to one the first export will be override!!!**
+* **If there a 2 export tags in one file with the same destination it will be merged!!!**
---
+++ jquery.stylehistroy.js
@@ -0,0 +1,216 @@
+/* stylehistory.js v.0.0.1
+ ** http://stephanjaeger-public.sharepoint.com
+ ** stephan@stephanjaeger.onmicrosoft.com
+ **
+ ** Copyright (c) 2015 Stephan Jäger
+ ** Available under the MIT license
+ */
+jQuery.fn.stylehistory=function(options){
+    $(window).resize(function(){
+        var cW=$('.'+sh).width;
+        if(cW>$(window).width){
+            $('.'+sh).width=$(window).width;
+        }
+    });
+    var v=jQuery.extend({
+        chronologically:true,reverse:false,itemCount:0,marker:'dot'
+    },options);
+    var sh="style-history";
+    var shi="style-history-item";
+    var shiy="style-history-item-year";
+    var shim="style-history-item-month";
+    var shit="style-history-item-text";
+    var shii="style-history-inner-item";
+    var shiiy="style-history-inner-item-year";
+    var shiim="style-history-inner-item-month";
+    var shiimoi="style-history-inner-item-mo-inner";
+    var shiimi="style-history-inner-item-month-inner";
+    var shiimim="style-history-inner-item-month-inner-month";
+    var po;
+    var dot='<div class="dot"></div>';
+    var aup='<div class="arrow up"></div>';
+    var ado='<div class="arrow down"></div>';
+    var sep='<div class="septor"></div>';
+    var iA=[];
+    var tA=[];
+    var mA=[];
+    var j;
+    var mo;
+    var mM=false;
+
+    function init(){
+        $.each($('.'+shi),function(index,div){
+            if(div.children.length>0){
+                var y="";
+                var m="";
+                $.each(div.children,function(i,d){
+                    if(d.className==shiy){
+                        if(!mM){
+                            y=d.innerHTML;
+                        }
+                    }
+                    if(d.className==shim){
+                        if(mM){
+                            mM=false;
+                            mA.push({m:m,t:tA});
+                            tA=[];
+                        }
+                        m=d.innerHTML;
+                        var l=d.nextElementSibling;
+                        while(l!=null){
+                            if(l.className==shim){
+                                mM=true;
+                                break;
+                            }
+                            l=l.nextElementSibling;
+                        }
+                        var n=d.nextElementSibling;
+                        if(n.className==shit){
+                            tA.push(n.innerHTML);
+                            if(n.nextElementSibling!=null){
+                                while(n.nextElementSibling!=null&&n.nextElementSibling.className==shit){
+                                    if(n.nextElementSibling!=null){
+                                        n=n.nextElementSibling;
+                                        tA.push(n.innerHTML);
+                                    }
+                                    else{
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if(d.className==shit){
+                        if(!mM){
+                            if(tA.length==0){
+                                if(mA.length==0){
+                                    tA.push(d.innerHTML);
+                                }
+                            }
+                            if(mA.length==0){
+                                mA.push({m:m,t:tA});
+                                tA=[];
+                            }
+                        }
+                    }
+                });
+                if(tA.length>0){
+                    mA.push({m:m,t:tA})
+                }
+                iA.push({y:y,m:mA});
+                tA=[];
+                mA=[];
+            }
+        });
+    }
+
+    function sD(){
+        if(v.marker=='dot'){
+            po=dot;
+        }
+        else if(v.marker=='arrow'){
+            if(!v.reverse){
+                po=aup;
+            }
+            else
+            {
+                po=ado;
+            }
+        }
+    }
+
+    function cV(){
+        sD();
+        $.each(iA,function(index,item){
+            if(v.itemCount==0){
+                j=item.y;
+                $('.'+sh).append(po+'<div class="'+shii+'"><div class="'+shiiy+'">'+j+'</div><div class="'+shiim+j+'"></div>'+sep+'</div>');
+                $.each(item.m,function(i,m){
+                    mo=m.m;
+                    $("."+shiim+j+"").append('<div class="'+shiimi+j+'"><div class='+shiimi+j+'-month'+mo+'><div class="'+shiimim+'">'+(m.m==undefined?"":m.m)+'</div></div></div>');
+                    $.each(m.t,function(ind,t){
+                        $("."+shiimi+j+'-month'+mo+"").append('<div class="'+shiimi+j+'-text'+mo+'">'+(t==undefined?"":t)+'</div>');
+                    });
+                });
+            }
+            else{
+                if(index<v.itemCount){
+                    j=item.y;
+                    $('.style-history').append(po+'<div class="'+shii+'"><div class="'+shiiy+'">'+j+'</div><div class="'+shiim+j+'"></div>'+sep+'</div>');
+                    $.each(item.m,function(i,m){
+                        mo=m.m;
+                        $("."+shiim+j+"").append('<div class="'+shiimi+j+'"><div class='+shiimi+j+'-month'+mo+'><div class="'+shiimim+'">'+(m.m==undefined?"":m.m)+'</div></div></div>');
+                        $.each(m.t,function(ind,t){
+                            $("."+shiimi+j+'-month'+mo+"").append('<div class="'+shiimoi+j+'-text'+mo+'">'+(t==undefined?"":t)+'</div>');
+                        });
+                    });
+                }
+            }
+        });
+        $('.'+shiiy+'').css({
+            'float':'left'
+        });
+        $('.'+shiim).css({
+            'float':'left'
+        });
+    }
+
+    function gf(a,b){
+        return b-a;
+    }
+
+    function sf(a,b){
+        return a-b;
+    }
+
+    function cA(){
+        iA.sort(function(a,b){
+            var r;
+            if(v.reverse){
+                r=sf(parseInt(a.y),parseInt(b.y));
+            }
+            else{
+                r=gf(parseInt(a.y),parseInt(b.y));
+            }
+            return r;
+        });
+    }
+
+    function cY(){
+        $.each(iA,function(i,d){
+            if(d.m.length>1){
+                d.m.sort(function(a,b){
+                    var r;
+                    if(v.reverse){
+                        r=sf(parseInt(a.m),parseInt(b.m));
+                    }
+                    else{
+                        r=gf(parseInt(a.m),parseInt(b.m));
+                    }
+                    return r;
+                });
+            }
+        })
+        iA.sort(function(a,b){
+            var r;
+            r=sf(parseInt(a.y),parseInt(b.y));
+            return r;
+        });
+    }
+
+    function hOV(){
+        $('.'+shi).remove();
+    }
+
+    return this.each(function(){
+        if($(this).length>0){
+            init();
+            if(v.chronologically){
+                cY();
+                cA();
+            }
+            cV();
+            hOV();
+        }
+    });
+};
