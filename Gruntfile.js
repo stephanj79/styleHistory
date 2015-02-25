d1452e7d5741c826f5d6bcf49d407eafbe6d46ff
stephanj79 <jaeger1779@gmail.com>
2015-02-25 16:16:06 +0100

    Gruntfile banner hinzugefügt min.js banner hinzugefügt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,10 @@
 module.exports=function(grunt){
+    var banner="/* stylehistory.js v.1.0.1\n** http://stephanjaeger-public.sharepoint.com\n** stephan@stephanjaeger.onmicrosoft.com\n** https://github.com/stephanj79/styleHistory\n** https://www.npmjs.com/package/stylehistory\n** Live-demo: http://jsfiddle.net/stephanj79/g7631mhf/\n** Copyright (c) 2015 Stephan Jäger\n** Available under the MIT license\n** https://github.com/stephanj79/styleHistory/blob/master/LICENSE-MIT\n*/";
     grunt.initConfig({
         uglify:{
             dist:{
                 options:{
-                    banner:'<%= banner %>',sourcesContent:true
+                    banner:banner,sourcesContent:true
                 },files:{
                     'jquery.stylehistroy.min.js':'jquery.stylehistroy.js'
                 }
